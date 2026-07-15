52def615150ee7df401da6ab4102d4f2d8e409ec
Ranenter <ranenter7@proton.me>
2026-07-15 11:16:27 +0200

    Add files via upload

---
+++ data/data-martin.js
@@ -0,0 +1,167 @@
+// ══ DATA: MARTIN ══
+window.VAA_DATA = window.VAA_DATA || {};
+window.VAA_DATA.candidates = window.VAA_DATA.candidates || {};
+window.VAA_DATA.questions = window.VAA_DATA.questions || {};
+
+// ── Kandidáti: Primátor Martina ──
+window.VAA_DATA.candidates['martin_primator'] = [
+  {id:'uhercik', name:'Ing. Michal Uherčík', party:'SaS / PS / KDH / Hnutie Slovensko / Demokrati / OKS', color:'#2196F3', url:'https://www.aktuality.sk/clanok/kQaY7Y2/volby-2026-v-martine-ohlasili-prveho-kandidata-na-primatora-stavila-nanho-cela-opozicia/'},
+  {id:'danko', name:'Ján Danko', party:'Nezávislý', color:'#4caf50', url:'https://www.martin.sk/jan-danko/o-1240'},
+  {id:'turzo', name:'Matej Turzo', party:'Nezávislý', color:'#ff9800', url:'https://www.matejturzo.sk/'},
+  {id:'ftorek', name:'Milan Ftorek', party:'Reštart Martina / nezávislý', color:'#9c27b0', url:'https://www.restartmartina.sk/'},
+  {id:'hubacek', name:'Igor Hubáček', party:'Republika / nezávislý', color:'#795548', url:''}
+];
+
+// ── Otázky: Primátor Martina ──
+window.VAA_DATA.questions.martin_primator = [
+  {
+    t:'Doprava a parkovanie',
+    q:'Má Martin rozšíriť regulované parkovanie aj do ďalších častí mesta?',
+    w:'Parkovanie patrí medzi najspornejšie mestské témy v Martine. Regulácia môže pomôcť rezidentom a uvoľniť miesta v preťažených lokalitách, no zároveň prináša nové poplatky a pravidlá.',
+    a:{uhercik:'agree', danko:'neutral', turzo:'neutral', ftorek:'disagree', hubacek:'disagree'}
+  },
+  {
+    t:'Doprava a parkovanie',
+    q:'Má ísť väčšina výnosov z parkovania späť do dopravy, ciest a parkovania?',
+    w:'Ak mesto vyberá peniaze za parkovanie, časť obyvateľov očakáva, že ich má viditeľne vracať späť do ulíc, parkovacích kapacít, verejnej dopravy a bezpečnosti.',
+    a:{uhercik:'agree', danko:'neutral', turzo:'agree', ftorek:'agree', hubacek:'agree'}
+  },
+  {
+    t:'Rezidenti',
+    q:'Majú mať obyvatelia v mieste bydliska výrazne zvýhodnené rezidentské parkovanie?',
+    w:'Zvýhodnenie rezidentov pomáha domácnostiam v preťažených uliciach. Na druhej strane môže skomplikovať fungovanie návštev, služieb a prevádzok v širšom centre.',
+    a:{uhercik:'agree', danko:'agree', turzo:'agree', ftorek:'agree', hubacek:'agree'}
+  },
+  {
+    t:'Centrum mesta',
+    q:'Má Martin viac obmedziť autá v centre, aj keď to sťaží prístup časti vodičov?',
+    w:'Menej áut v centre môže pomôcť peším, verejnému priestoru a kvalite ovzdušia. Kritici však upozorňujú na horší prístup autom pre časť obyvateľov, návštevníkov a podnikateľov.',
+    a:{uhercik:'agree', danko:'neutral', turzo:'agree', ftorek:'agree', hubacek:'disagree'}
+  },
+  {
+    t:'Cyklodoprava',
+    q:'Má mesto ďalej budovať cyklotrasy aj na úkor časti parkovania alebo jazdných pruhov?',
+    w:'Cyklotrasy zlepšujú bezpečnosť a ponúkajú inú možnosť dopravy než auto. V uliciach s obmedzeným priestorom však často znamenajú konflikt s parkovaním alebo s dopravou áut.',
+    a:{uhercik:'agree', danko:'neutral', turzo:'agree', ftorek:'agree', hubacek:'disagree'}
+  },
+  {
+    t:'Bezpečnosť',
+    q:'Má Martin častejšie budovať vyvýšené priechody, zóny 30 a spomaľovacie prvky?',
+    w:'Takéto opatrenia zvyšujú bezpečnosť chodcov, detí a seniorov. Časť vodičov ich však vníma ako zhoršenie plynulosti dopravy a vyšší hluk.',
+    a:{uhercik:'agree', danko:'agree', turzo:'agree', ftorek:'agree', hubacek:'neutral'}
+  },
+  {
+    t:'Sídliská vs. centrum',
+    q:'Má ísť viac mestských investícií zo centra do sídlisk a okrajových častí Martina?',
+    w:'Časť obyvateľov chce reprezentatívne centrum, iní očakávajú viac peňazí na chodníky, vnútrobloky, cesty a ihriská v obytných častiach mesta.',
+    a:{uhercik:'agree', danko:'neutral', turzo:'agree', ftorek:'agree', hubacek:'agree'}
+  },
+  {
+    t:'Nemocnica',
+    q:'Má mesto aktívne podporovať novú univerzitnú nemocnicu aj za cenu, že jej bude musieť podriadiť časť dopravy, územných rozhodnutí a investičných priorít?',
+    w:'Nová Univerzitná nemocnica Martin patrí medzi najväčšie verejné projekty v meste a jej výstavba zasahuje do dopravy, povoľovacích procesov aj fungovania okolia. Podpora projektu môže mestu priniesť silnejšie zdravotnícke centrum a rozvoj, ale zároveň môže odsunúť časť iných mestských priorít.',
+    a:{uhercik:'agree', danko:'agree', turzo:'neutral', ftorek:'neutral', hubacek:'neutral'}
+  },
+  {
+    t:'Transparentnosť',
+    q:'Má Martin zverejňovať konečné ceny, meškania a dodatky pri veľkých projektoch v jednoduchej verejnej tabuľke?',
+    w:'Takýto prehľad by ľuďom uľahčil kontrolu hospodárenia mesta. Zároveň by zvyšoval tlak na vedenie mesta pri každom posune termínu alebo zmene ceny.',
+    a:{uhercik:'agree', danko:'neutral', turzo:'agree', ftorek:'agree', hubacek:'agree'}
+  },
+  {
+    t:'Zákazky',
+    q:'Má mesto pri verejných zákazkách uprednostniť kvalitu pred najnižšou cenou?',
+    w:'Najnižšia cena nemusí byť najlepšie riešenie, najmä pri opravách, stavbách a údržbe. Vyššia kvalita však môže znamenať aj vyššie počiatočné náklady.',
+    a:{uhercik:'agree', danko:'neutral', turzo:'agree', ftorek:'agree', hubacek:'neutral'}
+  },
+  {
+    t:'Mestský majetok',
+    q:'Má mesto zverejniť jasné pravidlá prenájmu mestských priestorov a majetku?',
+    w:'Mestské priestory a majetok môžu slúžiť podnikaniu, službám aj komunitným aktivitám. Jasné pravidlá by zvýšili predvídateľnosť a dôveru v rozhodovanie samosprávy.',
+    a:{uhercik:'agree', danko:'neutral', turzo:'agree', ftorek:'agree', hubacek:'agree'}
+  },
+  {
+    t:'Bývanie',
+    q:'Má Martin stavať mestské nájomné byty aj za cenu vyššieho zadlženia alebo presunu peňazí z iných projektov?',
+    w:'Dostupné bývanie je problém aj v stredne veľkých mestách. Mestské nájomné byty môžu pomôcť mladým rodinám a profesiám potrebným pre mesto, no vyžadujú vysoké investície.',
+    a:{uhercik:'agree', danko:'neutral', turzo:'agree', ftorek:'agree', hubacek:'neutral'}
+  },
+  {
+    t:'Bývanie',
+    q:'Má mesto obmedziť novú výstavbu, ak príliš zaťažuje dopravu, školy a infraštruktúru?',
+    w:'Nová výstavba prináša byty a rast mesta, ale môže zhoršiť dopravu, tlak na parkovanie a kapacity škôl či škôlok.',
+    a:{uhercik:'agree', danko:'neutral', turzo:'agree', ftorek:'agree', hubacek:'agree'}
+  },
+  {
+    t:'Životné prostredie',
+    q:'Majú pri rekonštrukciách ulíc pribúdať stromy a zeleň aj na úkor parkovania?',
+    w:'Viac zelene zlepšuje tieň, mikroklímu a kvalitu verejného priestoru. V hustejších uliciach to však môže znamenať menej parkovacích miest.',
+    a:{uhercik:'agree', danko:'neutral', turzo:'agree', ftorek:'agree', hubacek:'disagree'}
+  },
+  {
+    t:'Energetika',
+    q:'Má Martin viac investovať do znižovania energetickej spotreby mestských budov, aj keď výsledky nebudú vidieť okamžite?',
+    w:'Energetické úspory môžu mestu časom ušetriť peniaze, no bývajú menej viditeľné než nové stavby alebo obnovy verejných priestorov.',
+    a:{uhercik:'agree', danko:'agree', turzo:'agree', ftorek:'agree', hubacek:'neutral'}
+  },
+  {
+    t:'Školstvo',
+    q:'Majú mať školy a škôlky prednosť pred časťou veľkých mestských projektov?',
+    w:'Investície do škôl a škôlok sa priamo dotýkajú rodín s deťmi. Zároveň súťažia o peniaze s dopravou, športom, verejnými priestormi a kultúrou.',
+    a:{uhercik:'agree', danko:'neutral', turzo:'agree', ftorek:'agree', hubacek:'agree'}
+  },
+  {
+    t:'Mládež a odliv ľudí',
+    q:'Má mesto cielene investovať do bývania, služieb a prostredia pre mladé rodiny, aby z Martina neodchádzali?',
+    w:'Mnohé stredne veľké mestá riešia odliv mladých ľudí do väčších centier alebo do zahraničia. Odpoveďou môžu byť dlhodobé investície do bývania, škôl a služieb.',
+    a:{uhercik:'agree', danko:'neutral', turzo:'agree', ftorek:'agree', hubacek:'agree'}
+  },
+  {
+    t:'Kultúra',
+    q:'Má Martin viac investovať do kultúry a mestských podujatí aj v časoch rozpočtového tlaku?',
+    w:'Martin má silnú kultúrnu identitu a kultúrne podujatia sú súčasťou jeho obrazu. Kritici však môžu tvrdiť, že v ťažších časoch majú mať prednosť praktickejšie výdavky.',
+    a:{uhercik:'agree', danko:'agree', turzo:'agree', ftorek:'agree', hubacek:'neutral'}
+  },
+  {
+    t:'Šport',
+    q:'Má mesto uprednostniť obnovu športovísk a športovej infraštruktúry pred časťou iných investícií?',
+    w:'Športoviská ovplyvňujú voľný čas rodín, mládeže aj klubov. Zároveň však súťažia o rozpočet s dopravou, školami a verejnými priestormi.',
+    a:{uhercik:'neutral', danko:'agree', turzo:'agree', ftorek:'agree', hubacek:'agree'}
+  },
+  {
+    t:'Podnikanie',
+    q:'Má mesto viac zvýhodniť malé miestne prevádzky a podnikateľov pred veľkými investormi?',
+    w:'Malé prevádzky držia život v meste a lokálnu ekonomiku. Veľkí investori však môžu priniesť vyšší objem pracovných miest a väčší ekonomický efekt.',
+    a:{uhercik:'agree', danko:'neutral', turzo:'agree', ftorek:'agree', hubacek:'neutral'}
+  },
+  {
+    t:'Priemysel a zamestnávatelia',
+    q:'Majú veľkí zamestnávatelia viac prispievať na dopravu, bývanie alebo služby v meste?',
+    w:'Veľkí zamestnávatelia profitujú z mestskej infraštruktúry a zároveň ju zaťažujú. Tvrdšie požiadavky môžu byť spravodlivé, ale časť investorov by mohli odrádzať.',
+    a:{uhercik:'agree', danko:'neutral', turzo:'agree', ftorek:'agree', hubacek:'disagree'}
+  },
+  {
+    t:'Sociálna politika',
+    q:'Má Martin posilniť sociálne služby a podporu seniorov aj za cenu menších investícií inde?',
+    w:'Starnutie obyvateľstva zvyšuje tlak na sociálne služby, opatrovanie a zariadenia pre seniorov. Každé posilnenie tejto oblasti však môže obmedziť priestor pre iné investície.',
+    a:{uhercik:'agree', danko:'agree', turzo:'agree', ftorek:'agree', hubacek:'agree'}
+  },
+  {
+    t:'Komunikácia s obyvateľmi',
+    q:'Má vedenie mesta robiť pravidelné verejné stretnutia v jednotlivých častiach Martina?',
+    w:'Pravidelné stretnutia môžu zlepšiť komunikáciu medzi mestom a obyvateľmi. Zároveň vytvárajú väčší tlak na vedenie mesta verejne vysvetľovať aj nepopulárne rozhodnutia.',
+    a:{uhercik:'agree', danko:'neutral', turzo:'agree', ftorek:'agree', hubacek:'agree'}
+  },
+  {
+    t:'Participácia',
+    q:'Má mesto častejšie využívať participatívne rozhodovanie alebo miestne hlasovania pri veľkých sporoch?',
+    w:'Silnejšie zapojenie obyvateľov môže zvýšiť dôveru a legitimitu veľkých rozhodnutí. Kritici však upozorňujú, že môže spomaliť rozhodovanie a zjednodušiť zložité odborné témy.',
+    a:{uhercik:'agree', danko:'neutral', turzo:'agree', ftorek:'agree', hubacek:'neutral'}
+  },
+  {
+    t:'Smerovanie mesta',
+    q:'Potrebuje Martin po roku 2026 výraznejšiu zmenu štýlu vedenia mesta než doteraz?',
+    w:'Komunálne voľby nerozhodujú len o projektoch, ale aj o tom, či má mesto pokračovať v doterajšom štýle riadenia, alebo zvoliť výraznejšiu zmenu smerovania a priorít.',
+    a:{uhercik:'agree', danko:'disagree', turzo:'agree', ftorek:'agree', hubacek:'agree'}
+  }
+];
